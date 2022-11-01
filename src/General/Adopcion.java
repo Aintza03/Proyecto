3424be277b7717c69c4848b98c44dcd04f55118b
discretemathlover <116070234+discretemathlover@users.noreply.github.com>
2022-11-01 17:01:33 +0100

    Create Adopcion.java

---
+++ src/General/Adopcion.java
@@ -0,0 +1,63 @@
+package General;
+
+import java.sql.Date;
+import java.util.Objects;
+
+
+public class Adopcion extends Servicio {
+	protected String contrato;
+
+	public Adopcion(Date fechaInicio, Animal animal) {
+		super(fechaInicio, animal);
+		this.fechaInicio = fechaInicio;
+		this.animal = animal;
+		this.contrato= null;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Animal getAnimal() {
+		return animal;
+	}
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+	
+	public String getContrato() {
+		return contrato;
+	}
+	public void setContrato(String contrato) {
+		this.contrato = contrato;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(contrato);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adopcion other = (Adopcion) obj;
+		return Objects.equals(contrato, other.contrato);
+	}
+	@Override
+	public String toString() {
+		return "Adopcion [contrato=" + contrato + "]";
+	}
+	
+	
+	
+	
+
+}
