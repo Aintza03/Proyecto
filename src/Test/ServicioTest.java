5a9549263c597d18523e07500cdcfaee7c564b34
discretemathlover <116070234+discretemathlover@users.noreply.github.com>
2022-11-01 17:22:41 +0100

    Create ServicioTest.java

---
+++ src/Test/ServicioTest.java
@@ -0,0 +1,49 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+
+import org.junit.Test;
+
+public class ServicioTest {
+	protected Servicio servicio;
+	protected Date fechaInicio;
+	protected Animal animal;
+	
+
+	@Test
+	public void testServicio() {
+		assertNotNull(servicio);
+		assertEquals(fechaInicio, servicio.getFechaInicio());
+		assertEquals(animal,servicio.getAnimal());
+	}
+
+	@Test
+	public void testGetFechaInicio() {
+		assertEquals(fechaInicio, servicio.getFechaInicio());
+	}
+
+	@Test
+	public void testSetFechaInicio() {
+		Date o = null;
+		servicio.setFechaInicio(o);
+		assertEquals(o,servicio.getFechaInicio());
+	}
+
+	@Test
+	public void testGetAnimal() {
+		assertEquals(animal,servicio.getAnimal());
+	}
+
+	@Test
+	public void testSetAnimal() {
+		Animal an = new Animal(0, null, null, null, null);
+		servicio.setAnimal(an);
+		assertEquals(an,servicio.getAnimal());
+	}
+
+	
+
+}
