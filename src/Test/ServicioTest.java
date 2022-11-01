b8c5fb3853acab32f38c871f84c117445256aff6
Ego818 <egoitz.martin@opendeusto.es>
2022-11-01 11:23:57 +0100

    Create ServicioTest.java

---
+++ src/Test/ServicioTest.java
@@ -0,0 +1,59 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ServicioTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testServicio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetFecha() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetFecha() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAnimales() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetAnimales() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
